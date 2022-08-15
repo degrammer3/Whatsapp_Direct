8f6f364faf42f90ff40bf681829c07421a73c58f
degrammer3 <dipakch93@gmail.com>
2022-08-16 00:58:35 +0530

    Update 15 August with some visual changes.

--- app/src/main/java/com/byteforce/whatsappdirect/MainActivity.kt
+++ app/src/main/java/com/byteforce/whatsappdirect/MainActivity.kt
@@ -14,15 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val number = findViewById<TextInputEditText>(R.id.contact)
-        val chat = findViewById<Button>(R.id.button)
+        val chat = findViewById<Button>(R.id.chatbutton)
+
         chat.setOnClickListener()
         {
             val store = number.text.toString()
             if(store!="") {
                 if (store.length<10) {
                     val alert = AlertDialog.Builder(this)
-                    alert.setTitle("Wait")
+                    alert.setTitle("Please Check")
                     alert.setMessage("Number is less than 10 Digits")
+                    alert.setIcon(R.mipmap.wadirect)
+                    val alertDialog:AlertDialog = alert.create()
+                    alertDialog.show()
+                }
+                else if(store.length>10)
+                {
+                    val alert = AlertDialog.Builder(this)
+                    alert.setTitle("Wait")
+                    alert.setMessage("Number is greater than 10 Digits")
+                    alert.setIcon(R.mipmap.wadirect)
                     val alertDialog:AlertDialog = alert.create()
                     alertDialog.show()
                 }
@@ -41,6 +52,7 @@ class MainActivity : AppCompatActivity() {
                 val alert = AlertDialog.Builder(this)
                 alert.setTitle(R.string.errortitle)
                 alert.setMessage(R.string.errormessage)
+                alert.setIcon(R.mipmap.wadirect)
                 val alertDialog:AlertDialog = alert.create()
                 alertDialog.show()
             }
